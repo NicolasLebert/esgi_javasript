a5305fe13ccdae3323f02c87cfca3a4deea43009
NicolasLebert <lebert.nicolas@yahoo.fr>
2019-11-28 10:57:49 +0100

    nouveau commit

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -9,17 +9,19 @@ function ucfirst(string) {
 console.log(ucfirst("hello wolrd!"));
 
 function camelCase(string) {
+    string = string.toLowerCase();
     do {
-        let pos = string.indexOf(" ") + 1;
+        let pos;
+        string.indexOf(" ") == -1 ? pos = string.indexOf("_") + 1 : pos = string.indexOf(" ") + 1;
         let letterToUpp = string.substr(pos, 1);
         string = string.substring(0, pos) + string.substring(pos).replace(letterToUpp, letterToUpp.toUpperCase());
         
         string = string.replace(" ", "");
-    }while (string.indexOf(" ") != -1)
+    }while (string.indexOf(" ") != -1 && string.indexOf("_") != -1)
     return ucfirst(string);
 }
 
-console.log(camelCase("salut a tous je suis nicolas lebert"));
+console.log(camelCase("saLut a tous_je_suis nicolas lebert"));
 
 function prop_access(obj, gateway) {
     
